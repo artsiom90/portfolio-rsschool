728c7fe030fae497812f214ee093a47dacc6c6e4
artsiom90 <artsiom.trush@gmail.com>
2022-01-16 23:17:37 +0300

    feat: implement image change

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,3 +1,4 @@
+// menu
 const burgerMenuButton = document.querySelector('.burger-menu')
 const closeMenuButton = document.querySelector('.nav-close-menu')
 const menu = document.querySelector('.nav')
@@ -20,3 +21,38 @@ const closeMenu = e => {
 }
 
 navLinks.forEach(link => link.addEventListener('click', closeMenu))
+
+
+// portfolio
+const portfolioBtns = document.querySelectorAll('.portfolio-buttons')
+const portfolioBtn = document.querySelectorAll('.portfolio-button')
+const portfolioImages = document.querySelectorAll('.portfolio-image')
+
+const preloadImages = () => {
+    const seasons = ['winter', 'spring', 'summer', 'autumn']
+
+    seasons.forEach(season => {
+        for (let i = 1; i <= 6; i++) {
+            const img = new Image()
+            img.src = `./assets/img/${season}/${i}.jpg`
+        }
+    })
+}
+
+preloadImages()
+
+const changeImages = e => {
+    if (e.target.classList.contains('portfolio-button')) {
+        portfolioImages.forEach((img, i) => img.src = `./assets/img/${e.target.dataset.season}/${i + 1}.jpg`)
+    }
+    portfolioBtn.forEach(btn => {
+        if (btn.classList.contains('button-gold')) {
+            btn.classList.remove('button-gold')
+            btn.classList.add('button-transparent')
+        }
+    })
+    e.target.classList.remove('button-transparent')
+    e.target.classList.add('button-gold')
+}
+
+portfolioBtns.forEach(btn => btn.addEventListener('click', changeImages))
