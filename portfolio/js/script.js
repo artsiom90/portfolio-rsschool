5418d0b07c033666b48f03f8b134702e5366d5b2
artsiom90 <artsiom.trush@gmail.com>
2022-01-20 17:57:11 +0300

    feat: add light mode

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -21,9 +21,7 @@ const closeMenu = e => {
         document.body.style.overflow = 'visible'
     }
 }
-
 navLinks.forEach(link => link.addEventListener('click', closeMenu))
-
 
 // portfolio
 const portfolioBtns = document.querySelectorAll('.portfolio-buttons')
@@ -32,7 +30,6 @@ const portfolioImages = document.querySelectorAll('.portfolio-image')
 
 const preloadImages = () => {
     const seasons = ['winter', 'spring', 'summer', 'autumn']
-
     seasons.forEach(season => {
         for (let i = 1; i <= 6; i++) {
             const img = new Image()
@@ -100,3 +97,108 @@ const getTranslate = (e, obj) => {
 }
 
 languagesBtns.forEach(btn => btn.addEventListener('click', e => getTranslate(e, i18Obj)))
+
+// theme
+const logo = document.querySelector('.logo')
+const themeBtn = document.querySelector('.theme-button')
+const containers = document.querySelectorAll('.container')
+const buttonWrapper = document.querySelectorAll('.button-wrapper')
+const sectionTitle = document.querySelectorAll('.section-title')
+const socialLinks = document.querySelectorAll('.footer-social-link')
+const headerContainer = document.querySelector('.header-container')
+const heroContainer = document.querySelector('.hero-container')
+const contactsConatainer = document.querySelector('.contacts-container')
+
+const changeContainerBackgroundImgDarkTheme = () => {
+    if (document.documentElement.clientWidth <= 768) {
+        headerContainer.style.backgroundImage = "url('./assets/img/image-bg-tablet.jpg')"
+        heroContainer.style.backgroundImage = "url('./assets/img/image-bg-tablet.jpg')"
+    } else if (document.documentElement.clientWidth > 768) {
+        headerContainer.style.backgroundImage = "url('./assets/img/image-bg.jpg')"
+        heroContainer.style.backgroundImage = "url('./assets/img/image-bg.jpg')"
+    }
+    contactsConatainer.style.backgroundImage = "url('./assets/img/contacts-bg.jpg')"
+}
+
+const changeContainerBackgroundImgLightTheme = () => {
+    if (document.documentElement.clientWidth <= 768) {
+        headerContainer.style.backgroundImage = "url('./assets/img/image-bg-tablet-light.jpg')"
+        heroContainer.style.backgroundImage = "url('./assets/img/image-bg-tablet-light.jpg')"
+    } else if (document.documentElement.clientWidth > 768) {
+        headerContainer.style.backgroundImage = "url('./assets/img/image-bg-light.jpg')"
+        heroContainer.style.backgroundImage = "url('./assets/img/image-bg-light.jpg')"
+    }
+    contactsConatainer.style.backgroundImage = "url('./assets/img/contacts-bg-light.jpg')"
+}
+
+const changeBackgroundSvgDarkTheme = () => {
+    logo.style.backgroundImage = "url('./assets/svg/logo.svg')"
+    themeBtn.style.backgroundImage = "url('./assets/svg/sun.svg')"
+    burgerMenuButton.style.backgroundImage = "url('./assets/svg/burger.svg')"
+    closeMenuButton.style.backgroundImage = "url('./assets/svg/close.svg')"
+    socialLinks.forEach((link, i) => link.style.backgroundImage = `url('./assets/svg/social/${i + 1}.svg')`)
+}
+
+const changeBackgroundSvgLightTheme = () => {
+    logo.style.backgroundImage = "url('./assets/svg/logo-black.svg')"
+    themeBtn.style.backgroundImage = "url('./assets/svg/moon-light.svg')"
+    burgerMenuButton.style.backgroundImage = "url('./assets/svg/burger-black.svg')"
+    closeMenuButton.style.backgroundImage = "url('./assets/svg/close-black.svg')"
+    socialLinks.forEach((link, i) => link.style.backgroundImage = `url('./assets/svg/social/${i + 1}-dark.svg')`)
+}
+
+const changeStylesDarkTheme = () => {
+    containers.forEach(container => container.classList.remove('theme-light-font'))
+    portfolioBtn.forEach(btn => btn.classList.remove('button-active-light'))
+    buttonWrapper.forEach(item => item.style.backgroundColor = '#000000')
+    sectionTitle.forEach(item => item.classList.remove('section-title-light'))
+    document.body.style.backgroundColor = '#000000'
+}
+
+const changeStylesLightTheme = () => {
+    containers.forEach(container => container.classList.add('theme-light-font'))
+    portfolioBtn.forEach(btn => btn.classList.add('button-active-light'))
+    buttonWrapper.forEach(item => item.style.backgroundColor = '#BDAE82')
+    sectionTitle.forEach(item => item.classList.add('section-title-light'))
+    document.body.style.backgroundColor = '#FFFFFF'
+}
+
+const setPropertyDarkTheme = () => {
+    document.documentElement.style.setProperty('--background-color-button', 'transparent')
+    document.documentElement.style.setProperty('--text-color-button-transparent', '#BDAE82')
+    document.documentElement.style.setProperty('--background-color-theme', '#000000')
+    document.documentElement.style.setProperty('--font-color-theme', '#FFFFFF')
+    document.documentElement.style.setProperty('--text-color-title', '#BDAE82')
+    document.documentElement.style.setProperty('--text-color-theme', '#BDAE82')
+    document.documentElement.style.setProperty('--text-color-button-gold', '#FFFFFF')
+    document.documentElement.style.setProperty('--background-color-form', 'rgba(0, 0, 0, 0.5)')
+}
+
+const setPropertyLightTheme = () => {
+    document.documentElement.style.setProperty('--background-color-button', '#BDAE82')
+    document.documentElement.style.setProperty('--text-color-button-transparent', '#1C1C1C')
+    document.documentElement.style.setProperty('--background-color-theme', '#FFFFFF')
+    document.documentElement.style.setProperty('--font-color-theme', '#1C1C1C')
+    document.documentElement.style.setProperty('--text-color-title', '#1C1C1C')
+    document.documentElement.style.setProperty('--text-color-theme', '#FFFFFF')
+    document.documentElement.style.setProperty('--text-color-button-gold', '#BDAE82')
+    document.documentElement.style.setProperty('--background-color-form', 'rgba(255, 255, 255, 0.5)')
+}
+
+const changeTheme = () => {
+    if (themeBtn.classList.contains('theme-light')) {
+        themeBtn.classList.remove('theme-light')
+        changeBackgroundSvgDarkTheme()
+        setPropertyDarkTheme()
+        changeContainerBackgroundImgDarkTheme()
+        changeStylesDarkTheme()
+    } else {
+        themeBtn.classList.add('theme-light')
+        changeBackgroundSvgLightTheme()
+        setPropertyLightTheme()
+        changeContainerBackgroundImgLightTheme()
+        changeStylesLightTheme()
+    }
+}
+
+themeBtn.addEventListener('click', changeTheme)
